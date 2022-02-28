58a16fd54cfd75c9f2b4e7d222da73811d6e2f0d
Thinh <th19776675@gmail.com>
2022-02-28 18:33:19 +0800

    Added in HTML, CSS and Javascript for main functionality

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,49 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+// Declare Inputs
+var upperCaseCheck = document.querySelector(".uppercase")
+var lowerCaseCheck = document.querySelector(".lowercase")
+var numericCheck = document.querySelector(".numeric")
+var specialCheck = document.querySelector(".special")
+var rangeSlider = document.querySelector(".range")
+
+// Declare Characters
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerChar = "abcdefghijklmnopqrstuvwxyz"
+var numericChar = "0123456789"
+var specialChar = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+
+
+//Generate Password
+function generatePassword () {
+  const settings = {
+    upperCase:upperCaseCheck.checked,
+    lowerCase:lowerCaseCheck.checked,
+    numeric:numericCheck.checked,
+    special:specialCheck.checked,
+    range:rangeSlider.value
+  }
+  let includedChar = "";
+  settings.upperCase ? (includedChar += upperChar) : "" ;
+  settings.lowerCase ? (includedChar += lowerChar) : "" ;
+  settings.numeric ? (includedChar += numericChar) : "" ;
+  settings.special ? (includedChar += specialChar) : "" ;
+  if (includedChar === "") {
+    alert("You must select at least one character.");
+  } else {
+    let password = "";
+    for (let i = 0; i < settings.range; i++) {
+      password += includedChar.charAt(
+        Math.floor(Math.random() * includedChar.length)
+      );
+    }
+    return password;
+  }
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +55,25 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Input Slider 
+const allRanges = document.querySelectorAll(".range-wrap");
+allRanges.forEach(wrap => {
+  const range = wrap.querySelector(".range");
+  const bubble = wrap.querySelector(".bubble");
+
+  range.addEventListener("input", () => {
+    setBubble(range, bubble);
+  });
+  setBubble(range, bubble);
+});
+
+function setBubble(range, bubble) {
+  const val = range.value;
+  const min = range.min ? range.min : 0;
+  const max = range.max ? range.max : 100;
+  const newVal = Number(((val - min) * 100) / (max - min));
+  bubble.innerHTML = val;
+  bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
+}
